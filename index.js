f22e3678a328a7ba99eb4cecbf5f567b18f4e568
Varis Karklins <vkarklins@outlook.com>
2026-07-20 17:49:28 -0700

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
