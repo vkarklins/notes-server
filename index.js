aa87bb457287835f409613203d5d3c0aadb61a7b
Varis Karklins <vkarklins@outlook.com>
2026-07-20 17:40:57 -0700

    Fixed PORT issue for deployment

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ app.post('/api/persons', (request, response) => {
   return response.status(204).end();
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT), () => {
+  console.log(`Server running on port ${PORT}`);
+};
